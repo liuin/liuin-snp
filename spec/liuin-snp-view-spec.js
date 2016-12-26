c0b29948e3e4f388239ae9366a2a248144be2016
liuin <20893791@qq.com>
2016-12-26 11:49:12 +0800

    1226

--- spec/liuin-snp-view-spec.js
+++ spec/liuin-snp-view-spec.js
@@ -4,6 +4,6 @@ import LiuinSnpView from '../lib/liuin-snp-view';
 
 describe('LiuinSnpView', () => {
   it('has one valid test', () => {
-    expect('life').toBe('easy');
+    //expect('life').toBe('easy');
   });
 });
